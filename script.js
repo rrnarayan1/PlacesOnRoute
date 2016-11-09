62b18b5583767b75ac92dac3e6c72ef26e4457b7
Rohan <rrnarayan1@gmail.com>
2016-11-09 01:24:03 -0800

    added step by step directions, support for transit waypoints, cleaned up html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsDisplay1 = new google.maps.DirectionsRenderer;
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 12,
@@ -24,7 +25,9 @@ function initMap() {
     });
 
     directionsDisplay.setMap(map);
-    
+    directionsDisplay1.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('right-right-panel'));
+    directionsDisplay1.setPanel(document.getElementById('right-below-panel'));
     
     var currentPos;
     var waypoints=[]
@@ -37,11 +40,15 @@ function initMap() {
     });
     document.getElementById('possibility0').addEventListener('click', function() {
         mode = checkMode()
-        calculateAndDisplayRoute(directionsService, directionsDisplay, clickTimePosition, currentPos, waypoints[0], mode);
+        document.getElementById('right-below-panel').innerHTML=""
+        directionsDisplay1.setDirections({routes: []})
+        calculateAndDisplayRoute(directionsService, directionsDisplay, directionsDisplay1, clickTimePosition, currentPos, waypoints[0], mode);
     });
     document.getElementById('possibility1').addEventListener('click', function() {
         mode = checkMode()
-        calculateAndDisplayRoute(directionsService, directionsDisplay, clickTimePosition, currentPos, waypoints[1], mode);
+        document.getElementById('right-below-panel').innerHTML=""
+        directionsDisplay1.setDirections({routes: []})
+        calculateAndDisplayRoute(directionsService, directionsDisplay, directionsDisplay1, clickTimePosition, currentPos, waypoints[1], mode);
     });
 }
 function checkMode(){
@@ -156,36 +163,59 @@ function changeHTMLWaypoints(possibleWaypoints, possiblePriorities){
 
 }
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay, posInit, currentPos, waypointPlace, mode) {
+function calculateAndDisplayRoute(directionsService, directionsDisplay, directionsDisplay1, posInit, currentPos, waypointPlace, mode) {
     var waypts = [];
     waypts.push({
         location: waypointPlace.geometry.location,
     })
-
-    directionsService.route({
-      origin: currentPos,
-      destination: posInit,
-      waypoints: waypts,
-      optimizeWaypoints: true,
-      travelMode: mode
-    }, function(response, status) {
-      if (status === 'OK') {
-        
-        directionsDisplay.setDirections(response);
-        var route = response.routes[0];
-        var summaryPanel = document.getElementById('directions-panel');
-        summaryPanel.innerHTML = '';
-        if(waypointPlace.name != null){
-            summaryPanel.innerHTML += "You're going to be stopping at "+waypointPlace.name+". "
-        } if(waypointPlace.vicinity != null){
-            summaryPanel.innerHTML += "The shop is located at "+waypointPlace.vicinity+". "
-        } if(waypointPlace.formatted_phone_number != null){
-            summaryPanel.innerHTML += "If you want to try ordering ahead, try calling here at "+
-            waypointPlace.formatted_phone_number+"."
-        }
-        summaryPanel.innerHTML += "<br>"
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
+    console.log(mode)
+    if(mode == 'TRANSIT'){
+        directionsService.route({
+          origin: currentPos,
+          destination: waypts[0],
+          optimizeWaypoints: true,
+          travelMode: mode
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          }
+        })
+        directionsService.route({
+          origin: waypts[0],
+          destination: posInit,
+          optimizeWaypoints: true,
+          travelMode: mode
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay1.setDirections(response);
+          }
+        })    
+    }
+    else{
+        directionsService.route({
+          origin: currentPos,
+          destination: posInit,
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: mode
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('directions-panel');
+            summaryPanel.innerHTML = '';
+            if(waypointPlace.name != null){
+                summaryPanel.innerHTML += "You're going to be stopping at "+waypointPlace.name+". "
+            } if(waypointPlace.vicinity != null){
+                summaryPanel.innerHTML += "The shop is located at "+waypointPlace.vicinity+". "
+            } if(waypointPlace.formatted_phone_number != null){
+                summaryPanel.innerHTML += "If you want to try ordering ahead, try calling here at "+
+                waypointPlace.formatted_phone_number+"."
+            }
+            summaryPanel.innerHTML += "<br>"
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+    }
 }
